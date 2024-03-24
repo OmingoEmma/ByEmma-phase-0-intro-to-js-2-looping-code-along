40a5a2d7c3e753d94fc4865edaa751e37df5ce89
OmingoEmma <emmaomingo@gmail.com>
2024-03-25 02:51:36 +0300

    Loops in Js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// index.js
+const writeCards = (names, message) => {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${message} gift!`)
+  }
+  
+  const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  }
